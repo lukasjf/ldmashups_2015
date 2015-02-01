1080a4f432346774b8612b466d677baffa8795fc
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-02-01 11:38:02 +0100

    Fixed DBpedia Query, Reset links of Species

--- ld_mashups2015/src/main/java/de/uni_potsdam/hpi/services/DBpediaService.java
+++ ld_mashups2015/src/main/java/de/uni_potsdam/hpi/services/DBpediaService.java
@@ -12,11 +12,12 @@ import de.uni_potsdam.hpi.data.SpeciesData;
 public class DBpediaService {
 
     private String sparqlEndpoint = "http://dbpedia.org/sparql";
-    private String queryTemplate = "SELECT ?name ?image ?abstract ?kingdom ?phylum ?family ?animal ?class ?order ?genus ?english_name " +
+    private String queryTemplate = "SELECT ?name ?image ?binomial ?abstract ?kingdom ?phylum ?family ?animal ?class ?order ?genus ?english_name " +
             "FROM <http://dbpedia.org> " +
             "WHERE { " +
-            "{{ ?animal <http://dbpedia.org/property/binomial> ?binomial FILTER <http://www.w3.org/2005/xpath-functions/#contains>(?binomial, \"%s\") } UNION " +
+            "{{ ?animal <http://dbpedia.org/property/binomial> \"%s\"@en} UNION " +
             "{ ?animal <http://dbpedia.org/property/synonyms> ?name FILTER <http://www.w3.org/2005/xpath-functions/#contains>(?name, \"%s\") }}" +
+            "?animal <http://dbpedia.org/property/binomial> ?binomial ." +
             "?animal <http://dbpedia.org/ontology/thumbnail> ?image . " +
             "?animal <http://dbpedia.org/ontology/abstract> ?abstract . " +
             "?animal <http://dbpedia.org/ontology/kingdom> ?kingdom ." +
@@ -32,7 +33,7 @@ public class DBpediaService {
 
     // searching for strings does not work yet, nor does it in the online endpoint
 	public void includeDataFromDBpedia(SpeciesData species){
-		String sparqlQueryString = String.format(queryTemplate, species.getBinomial(), species.getBinomial());
+	String sparqlQueryString = String.format(queryTemplate, species.getBinomial(), species.getBinomial());
         Query query = QueryFactory.create(sparqlQueryString);
         QueryExecution qexec = QueryExecutionFactory
           .sparqlService(sparqlEndpoint, query);
@@ -40,7 +41,7 @@ public class DBpediaService {
         if (results.hasNext()){
             QuerySolution answer = results.next();
             species.setdBpediaURI(answer.getResource("animal").getURI());
-            species.setDescription(answer.getLiteral("abstract").toString());
+            species.setDescription(answer.getLiteral("abstract").getString());
             species.setThumbnailURL(answer.getResource("image").getURI());     
             Resource phylum;
             if (null != (phylum = answer.getResource("phylum"))) {
@@ -53,8 +54,10 @@ public class DBpediaService {
             species.setGenus(answer.getResource("genus").getURI());
             if (answer.getLiteral("name") == null || "".equals(answer.getLiteral("name").toString())) {
                 species.setSynonyme(species.getBinomial());
+            } else if (null != answer.getLiteral("binomial")) {
+                species.setSynonyme(answer.getLiteral("binomial").getString());
             } else {
-                species.setSynonyme(answer.getLiteral("name").toString());
+                species.setSynonyme(answer.getLiteral("name").getString());
             }
             species.setName(answer.get("english_name").toString());
         }
